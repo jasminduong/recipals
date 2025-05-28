0da65733e7cdfcdbafdef54a050ca386431d47c0
Jasmin Duong <jasmind009@gmail.com>
2025-05-28 17:07:49 -0400

    edit post + home recipe post

--- src/ReciPals/Create/index.tsx
+++ src/ReciPals/Create/index.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+//import * as db from "../Database";
+
 export default function CreatePost() {
-    return (
-        <h1>Create Post</h1>
-    )
+  //const { rid } = useParams();
+  //const recipes = db.recipes;
+
+  //const recipeToEdit = recipes.find((recipe) => recipe._id === rid);
+
+  return (
+    <div>
+      <h1>Create Post</h1>
+        <Link to="/ReciPals/Recipes/Editor/recipe_bot-R1">
+          Edit Post UI
+        </Link>
+    </div>
+  );
 }
