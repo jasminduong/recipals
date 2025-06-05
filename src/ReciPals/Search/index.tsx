f2a596c4b3cbc044d49dc73b71d13630390bb1cf
daniel-jacob-16 <dankjacob4@gmail.com>
2025-06-05 19:18:25 -0400

    search results done

--- src/ReciPals/Search/index.tsx
+++ src/ReciPals/Search/index.tsx
@@ -1,35 +1,56 @@
-import { Container, FormControl } from "react-bootstrap";
+import { Col, Container, FormControl, Row } from "react-bootstrap";
 import { BiSearch } from "react-icons/bi";
 import * as db from "../Database";
 
 export default function Search() {
-  const posts = db.posts;
-
+  const recipes = db.recipes;
+// Filter down recipes, users, etc. based on user input (.contains()), then map those
     return (
-        <Container fluid className="mt-4 px-2 px-md-4" id="search">
-            <div className="position-relative me-3 d-flex align-items-center mb-4">
-                <FormControl type="search" placeholder="Search recipes, ingredients, users..." className="ps-5" />
-                <div className="position-absolute top-50 translate-middle-y ms-3 text-secondary">
-                    <BiSearch />
-                </div>
-             </div>
-             <div>
-{/* {posts.map((section) => (
-                  <div key={section._id} className="mb-3">
-                    <strong className="d-block mb-2">{section.title}</strong>
-                    {section["ingredients:"].map((ingredient, index) => (
-                      <Form.Control
-                        key={index}
-                        className="mb-2"
-                        defaultValue={ingredient}
-                      />
-                    ))}
-                    <Button className="recipe-editor-btn" style={{color: "#225593"}}>+ Add another ingredient</Button>
-                    <hr></hr>
+      <Container fluid className="mt-4 px-2 px-md-4" id="search">
+        <div className="position-relative me-3 d-flex align-items-center mb-4">
+          <FormControl type="search" placeholder="Search recipes, ingredients, users..." className="ps-5" />
+          <div className="position-absolute top-50 translate-middle-y ms-3 text-secondary">
+            <BiSearch />
+          </div>
+        </div>
+        <div>
+          {recipes.map((recipe) => (
+            <div>
+              <Row className="mt-5 ms-1 mb-5">
+                <Col xs={3} className="mb-3 mb-md-0">
+                  <img
+                    src={recipe.photo}
+                    className="img-fluid"
+                    style={{ 
+                      width: "200px", 
+                      height: "200px",
+                      objectFit: "cover" 
+                    }}
+                  />
+                </Col>
+                <Col xs={9}>
+                  <div className="recipe-body">
+                    {recipe.user_created}
                   </div>
-                ))} */}
-             </div>
-        </Container>
-        
+                  <div className="d-flex justify-content-between">
+                    <div className="recipe-title">
+                      {recipe.name}
+                    </div>
+                    <div className="recipe-sub-title">
+                      <b>Total Time:</b> {recipe.total_time}
+                    </div>
+                  </div>
+                  <div className="recipe-sub-title" >
+                      {recipe.description}
+                  </div>
+                </Col>
+              </Row>
+              <hr/>
+            </div>
+          ))}
+        </div>
+      </Container>   
     )
 }
+
+// posts.filter((input) => input.contains(post.created_by) || input.contains(posts.title))
